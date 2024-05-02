28203e0441b5ed0489efcca6196c3bed515719e7
Fitriawan-Arya-N <nugrahaaryaa17@gmail.com>
2024-05-02 20:32:14 +0700

    update

---
+++ src/notes.js
@@ -0,0 +1,5 @@
+//notes.js
+
+const notes = [];
+ 
+module.exports = notes;
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+// routes.js
+
+const { addNoteHandler, getAllNotesHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+];
+
+module.exports = routes;
